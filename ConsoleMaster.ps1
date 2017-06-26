9c9c95fa44eb08d0645dec694547d76832ce6fce
NovaViper <nova.gamez15+code@gmail.com>
2017-06-25 20:58:48 -0500

    Released v1.5, also added .gitignore for VIsual Studio Code

--- ConsoleMaster.ps1
+++ ConsoleMaster.ps1
@@ -1,36 +1,35 @@
 ﻿#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-# VARIABLE DECLARATION #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-#Get 
 $currentLocation = Split-Path -parent $PSCommandPath
 $ui = (Get-Host).UI.RawUI
 $consoleName = "Console Master"
-$consoleVersion = '1.0'
+$consoleVersion = '1.5.0'
 $prefix = "$consoleName $consoleVersion"
-$quitConsole = $false
+$QuitConsole = $false
 $configFileName = "config.json"
-$sleepTime = 4
+$SleepTime = 3
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-# POWERSHELL CUSTOMIZATION #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 $ui.WindowTitle = "$prefix"
 Set-Location $currentLocation
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-# FUNCTION DECLARATION #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
-function Setup-Console(){
+function Set-Up-Console() {
     Clear-Host
-    $ui.WindowTitle = "$prefix+: Server Running (Jar: , MC )"
-    Write-Host "Welcome To $consoleName!!"
+    $ui.WindowTitle = $prefix+": Server Running (Jar: , MC )"
+    Write-Host "Welcome To $prefix!!"
     Write-Host ''
-    
-    $jarFile = Check-Jar "Please enter the name of the server's .jar file (w/o the extension)"
-    $mcVersion = Check-Version "Please enter the Minecraft version your server is using"
-    $ramDataType = Check-RAM-Type
-    $maxRAM = Check-Integer "Please enter your maximum amount of RAM (Random Access Memory) to use"
+
+    $jarFile = Test-Jar "Please enter the name of the server's .jar file (w/o the extension)"
+    $mcVersion = Test-Version "Please enter the Minecraft version your server is using"
+    $ramDataType = Test-RAM-Type
+    $maxRAM = Test-Integer "Please enter your maximum amount of RAM (Random Access Memory) to use"
 
     $fullRAM = -Join ($maxRAM, $ramDataType)
     $basicInfo = @{
-        Jar_File = "$jarFile"
+        Jar_File          = "$jarFile"
         Minecraft_Version = "$mcVersion"
-        Java_Flags = "-Xmx$fullRAM"
-        Server_Options = "-o true"
-    
+        Java_Flags        = "-Xmx$fullRAM"
+        Server_Options    = "-o true"
+
     }
 
     New-Item $configFileName -ItemType "file" | Out-Null
@@ -38,144 +37,229 @@ function Setup-Console(){
 
     Write-Host ""
     Write-Host "Moving to Main Menu..." -BackgroundColor Red
-    Sleep -Seconds $sleepTime
-    Main-Menu
+    Start-Sleep -Seconds $SleepTime
+    Invoke-Main-Menu
 }
 
 
-function Main-Menu() {
-    Load-Variables
+function Invoke-Main-Menu() {
+    Update-Variables
     Clear-Host
     Write-Host @"
 ---------------------------------- Main Menu ----------------------------------
 
                     1. Start Server and Ngrok
                     2. Configurations Menu
-                        
+
                     3. Exit the Console
 
 -------------------------------------------------------------------------------
 "@
 
     $answer = Read-Host "Please Make a Selection"
-    switch($answer){
+    switch ($answer) {
         '1' {
             Clear-Host
             Start-Server
         }
         '2' {
             Clear-Host
-            Settings-Menu
+            Invoke-Settings-Menu
         }
         '3' {
             Exit-Script
         }
         default {
             Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
-            Main-Menu
+            Invoke-Main-Menu
         }
     }
 }
 
-function Start-Server(){
-    $consoleServerPrefix = "["+$consoleName+"]:"
-    $ui.WindowTitle = "$prefix+: Server Running (Jar: "+$Script:INFO.Jar_File+", MC "+$Script:INFO.Minecraft_Version+")"
+function Start-Server() {
+    $consoleServerPrefix = "[" + $consoleName + "]:"
+    $ui.WindowTitle = $prefix+": Server Running (Jar: " + $Script:INFO.Jar_File + ", MC " + $Script:INFO.Minecraft_Version + ")"
     Write-Host @"
 ------------------------------- SERVER LOG START -------------------------------
-$consoleServerPrefix Server Starting!
+$consoleServerPrefix Starting Server...
 "@
-    #-#-#-#-# Ngrok Checking here #-#-#-#-#
-    #Write-Host "$consoleServerPrefix Checking for Ngrok process..."
- 
-    ##Already Started
-    #Write-Host "$consoleServerPrefix Ngrok is Already Running! Skipping Ngrok Startup..."
- 
-    #Not Started
-    #Write-Host "$consoleServerPrefix Ngrok Not Running! Starting Ngrok..."
-    #start powershell {ngrok tcp 25565}
-    #Write-Host "$consoleServerPrefix Ngrok Started!"
-    #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
- 
-    Write-Host "$consoleServerPrefix Loading Java Arguments..."
-    #start-process java -ArgumentList '-Xms1G -Xmx1G -jar C:\Users\novag\Documents\Powershell\forge.jar' -Wait -NoNewWindow
-
+    Write-Host "$consoleServerPrefix Checking for Ngrok process..."
+    if (-not(Get-Process -Name ngrok -ErrorAction SilentlyContinue)) {
+        Write-Host "$consoleServerPrefix Ngrok Not Running! Starting Ngrok..."
+        Start-Process ngrok -argumentlist "tcp 25565" -passthru | Out-Null
+        Write-Host "$consoleServerPrefix Ngrok Started!"
+    }
+    else {
+        Write-Host "$consoleServerPrefix Ngrok is Already Running! Skipping Ngrok Startup..."
+    }
+    Write-Host "$consoleServerPrefix Starting .jar File Now.."
     java $INFO.Java_Flags -jar $INFO.Jar_File $INFO.Server_Options
 
+    $ui.WindowTitle = "$consoleServerPrefixServer Stopped (Jar: " + $Script:INFO.Jar_File + ", MC " + $Script:INFO.Minecraft_Version + ")"
+    Write-Host "$consoleServerPrefixServer Server Stopped! Check Above For Details!"
+    Invoke-Ask-Restart
+}
 
+function Invoke-Ask-Restart(){
+    $answer = Read-Host -Prompt "Would you like to restart the server? (Y)es/(N)o"
+    switch ($answer.ToUpper()) {
+        'Y' {
+            Write-Host $prefix+:"Restarting Server..." -BackgroundColor Red
+            Start-Sleep $SleepTime
+            Clear-Host
+            Start-Server
+        }
+        'N' {
+            Clear-Host
+            Confirm-Console-Close
+        }
+        default {
+            Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
+            Invoke-Ask-Restart
+        }
+    }
+}
+
+function Confirm-Console-Close () {
+    $ui.WindowTitle = $prefix + ': Confirm Exit'
+    $answer = Read-Host "Ok, would you to close"$consoleName"? (Y)es/(N)o"
+    switch ($answer.ToUpper()) {
+
+        'Y' {
+            Write-Host ""
+            Confirm-Ngrok-Close $true
+        }
+        'N' {
+            Write-Host ""
+            Confirm-Ngrok-Close $false
+        }
+        default {
+            Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
+            Confirm-Console-Close
+        }
+    }
 }
 
 
-function Settings-Menu() {
+function Confirm-Ngrok-Close([bool]$closeConsole) {
+    $answer = Read-Host 'Ok, would you to close Ngrok? (Y)es/(N)o'
+    switch ($answer.ToUpper()) {
+
+        'Y' {
+            Write-Host ""
+            Exit-Console-Ngrok $closeConsole $true
+        }
+        'N' {
+            Write-Host ""
+            Exit-Console-Ngrok $closeConsole $false
+        }
+        default {
+            Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
+            Confirm-Ngrok-Close
+        }
+    }
+}
+
+function Exit-Console-Ngrok([bool]$closeConsole, [bool]$closeNgrok) {
+
+    if (($closeConsole -eq $true) -and ($closeNgrok -eq $true)) {
+        Stop-Process -Name ngrok
+        Exit-Script
+    }
+    elseif (($closeConsole -eq $false) -and ($closeNgrok -eq $true)) {
+        Stop-Process -Name ngrok
+        Resume-Main-Menu
+    }
+    elseif (($closeConsole -eq $true) -and ($closeNgrok -eq $false)) {
+        Exit-Script
+    }
+    else {
+        Resume-Main-Menu
+    }
+}
+
+function Invoke-Settings-Menu() {
     Clear-Host
     Write-Host @"
 -------------------------------- Settings Menu --------------------------------
 
-    1. Change Version - Change the saved Minecraft version to match
+    1. Change Jar - Change the saved jar file name that the script
+                    executes
+    2. Change Version - Change the saved Minecraft version to match
                         what the jar file uses
-    2. RAM Allocation - Set the amount of RAM (Random Access Memory) 
+    3. RAM Allocation - Set the amount of RAM (Random Access Memory)
                         to reserve to the server [!]
-    3. Custom Arguments - Set custom arguments besides the RAM [!!]
-    4. Factory Reset - Erase all user defined settings and boot to 
-                        first-time setup [!!!]
+    4. Custom Arguments - Set custom arguments besides the RAM [!!]
+    5. Factory Reset - Erase all user defined settings and boot to
+                       first-time setup [!!!]
 
-    5. Return To Main Menu
+    6. Return To Main Menu
 
 -------------------------------------------------------------------------------
 "@
 
     $answer = Read-Host "Please Make a Selection"
-    switch($answer){
+    switch ($answer) {
         '1' {
             Clear-Host
-            Change-Minecraft-Version
+            Edit-Jar-File
         }
         '2' {
             Clear-Host
-            Change-RAM
+            Edit-Minecraft-Version
         }
         '3' {
             Clear-Host
-            Confirm-Change-Args
+            Edit-RAM
         }
         '4' {
             Clear-Host
+            Confirm-Change-Args
+        }
+        '5' {
+            Clear-Host
             Confirm-Factory-Reset
         }
-        '5' {Return-Main-Menu}
+        '6' {Resume-Main-Menu}
         default {
             Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
-            Settings-Menu
+            Invoke-Settings-Menu
         }
     }
 }
 
-function Change-Minecraft-Version(){
-    $newVersion = Check-Version "Please enter the new Minecraft version your server is using"
-    Save-Variable "Minecraft_Version" $newVersion
-    Return-Settings-Menu
+function Edit-Jar-File() {
+    $newJar = Test-Jar "Please enter the new name of the server's .jar file (w/o the extension)"
+    Save-Variable "Jar_File" $newJar
+    Resume-Settings-Menu
 }
 
-function Change-RAM(){
-    $newRamDataType = Check-RAM-Type
-    $newMaxRAM = Check-Integer "Please enter your maximum amount of RAM (Random Access Memory) to use"
+function Edit-Minecraft-Version() {
+    $newVersion = Test-Version "Please enter the new Minecraft version your server is using"
+    Save-Variable "Minecraft_Version" $newVersion
+    Resume-Settings-Menu
+}
+
+function Edit-RAM() {
+    $newRamDataType = Test-RAM-Type
+    $newMaxRAM = Test-Integer "Please enter your maximum amount of RAM (Random Access Memory) to use"
     $newFullRAM = -Join ($newMaxRAM, $newRamDataType)
     Save-Variable "Java_Flags" "-Xmx$newFullRAM"
-    Return-Settings-Menu
+    Resume-Settings-Menu
 }
 
-function Confirm-Change-Args(){
+function Confirm-Change-Args() {
     Write-Host "Here are your original arguments:" $Script:INFO.Server_Options
-    Write-Host "Are you SURE you want to modify these arguments?" -ForegroundColor Red
-    $answer = Read-Host "(Y)es or (N)o"
-    switch($answer.ToUpper()){
+    $answer = Read-Host "Are you SURE you want to modify these arguments? (Y)es/(N)o" -ForegroundColor Red
+    switch ($answer.ToUpper()) {
 
         'Y' {
             Clear-Host
-            Perform-Change-Args
+            Edit-Args
         }
         'N' {
             Clear-Host
-            Return-Settings-Menu
+            Resume-Settings-Menu
         }
         default {
             Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
@@ -184,35 +268,36 @@ function Confirm-Change-Args(){
     }
 }
 
-function Perform-Change-Args(){
+function Edit-Args() {
     Write-Host "As a refresher, here are your original arguments:" $Script:INFO.Server_Options
     $data = ''
-    While(($null -eq $data) -or ($data -eq '')){
+    While (($null -eq $data) -or ($data -eq '')) {
         [string]$data = Read-Host -Prompt "Please enter new arguments for your server"
-		
-        if (($null -eq $data) -or ($data -eq '')){
-			Write-Output ''
+
+        if (($null -eq $data) -or ($data -eq '')) {
+            Write-Output ''
             $data = ''
-			Invaild-Choice "You cannot enter a null/empty name!"
-		}else{
+            Invaild-Choice "You cannot enter a null/empty name!"
+        }
+        else {
             Save-Variable "Server_Options" $data
-            Return-Settings-Menu
-		}		
+            Resume-Settings-Menu
+        }
     }
 }
 
-function Confirm-Factory-Reset(){
+function Confirm-Factory-Reset() {
     Write-Host "Are you sure you want to reset back to factory default? This process CANNOT be reversed!!" -ForegroundColor Red
     $answer = Read-Host "(Y)es or (N)o"
-    switch($answer.ToUpper()){
+    switch ($answer.ToUpper()) {
 
         'Y' {
             Clear-Host
-            Perform-Factory-Reset
+            Initialize-Factory-Reset
         }
         'N' {
             Clear-Host
-            Return-Settings-Menu
+            Resume-Settings-Menu
         }
         default {
             Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
@@ -221,143 +306,139 @@ function Confirm-Factory-Reset(){
     }
 }
 
-function Perform-Factory-Reset(){
+function Initialize-Factory-Reset() {
     Write-Host "Deleting Preferences..." -BackgroundColor Red
     Remove-Item $configFileName
     Clear-Variable -name INFO
-    Write-Host "Factory Reset Completed!"
+    Write-Host "Factory Reset Completed!" -ForegroundColor Green
     Write-Host ""
     Write-Host "Returning to First-Time Setup..." -BackgroundColor Red
-    Sleep -Seconds $sleepTime
-    Setup-Console
+    Start-Sleep -Seconds $SleepTime
+    Set-Up-Console
 }
 
 
-function Exit-Script(){
+function Exit-Script() {
     Write-Host ""
     Write-Host 'Exiting Script...' -BackgroundColor Red
-    Start-Sleep -Seconds $sleepTime
-    $quitConsole = $true
+    Start-Sleep -Seconds $SleepTime
+    $QuitConsole = $true
 }
 
-function Return-Main-Menu(){
+function Resume-Main-Menu() {
     Write-Host ""
     Write-Host 'Returning to Main Menu...' -BackgroundColor Red
-    Sleep -Seconds $sleepTime
-    Main-Menu
+    Start-Sleep -Seconds $SleepTime
+    Invoke-Main-Menu
 }
 
-function Return-Settings-Menu(){
+function Resume-Settings-Menu() {
     Write-Host ""
     Write-Host "Returning to Settings Menu..." -BackgroundColor Red
-    Sleep -Seconds $sleepTime
-    Settings-Menu
+    Start-Sleep -Seconds $SleepTime
+    Invoke-Settings-Menu
 }
 
-function Invaild-Choice([string]$message){
+function Invaild-Choice([string]$message) {
     [Console]::Beep(1000, 100)
     Write-Host $message -ForegroundColor Red
-    Sleep -Seconds $sleepTime
+    Start-Sleep -Seconds $SleepTime
 }
 
-function Load-Variables(){
+function Update-Variables() {
     $Script:INFO = Get-Content -Path $configFileName -Raw | ConvertFrom-Json
 }
 
-function Save-Variable($keyName, $data){
+function Save-Variable($keyName, $data) {
     $Script:INFO.$keyName = $data
     $Script:INFO | ConvertTo-Json | Set-Content $configFileName
 }
 
-function Check-Jar([string]$promptMessage){
+function Test-Jar([string]$promptMessage) {
     $data = ''
-    While(($null -eq $data) -or ($data -eq '')){
+    While (($null -eq $data) -or ($data -eq '')) {
         [string]$data = Read-Host -Prompt $promptMessage
-		
-        if (($null -eq $data) -or ($data -eq '')){
-			Write-Output ''
-            $data = ''
-			Invaild-Choice "You cannot enter a null/empty name!"
-		}else{
-            return $data+'.jar'
-		}		
-    }
-}
 
-function Check-Version([string]$promptMessage){
-	$data = ''
-	While ($data -eq '')
-	{
-		$data = Read-Host -Prompt $promptMessage
-		$minVer = [version]'1.0.0'
-		$maxVer = [version]'999.999.999'
-
-		# not matching NOT 0-9 & '.' [ie - only 0-9 & '.']
-		#    must contain at least one '.'
-		#    must START with a digit
-		#    must END with a digit
-		if (($data -notmatch '[^0-9.]') -and ($data -match '\.') -and ($data[0] -match '[0-9]') -and ($data[-1] -match '[0-9]') -and ($data -ge $minVer) -and ($data -le $maxVer)){
-			return $data
-		}else{
+        if (($null -eq $data) -or ($data -eq '')) {
+            Write-Output ''
             $data = ''
-			Invaild-Choice "The value [$data] MUST be a version (major, minor, build) ranging between $minVer to $maxVer!"
-		}
-	}
-}
-
-function Check-Integer([string]$promptMessage){
-    $data = ''
-    $min = 1
-    $max = [int]::MaxValue
-    While(($null -eq $data) -or ($data -eq '') -or ($data -eq '0') -or (-not ($data -notmatch '[^\d]'))){
-       
-        [int]$data = Read-Host -Prompt $promptMessage
-        if(($null -eq $data) -or ($data -eq '') -or ($data -eq '0')){
-			Write-Host ''
-            $data = ''
-			Invaild-Choice "The value [$data] cannot be null, empty or 0!"
-		}elseif(-not ($data -notmatch '[^\d]')){
-			Write-Output ''
-            $data = ''
-			Invaild-Choice "The value [$data] MUST be an integer!"
-		}else{
-		    return $data
+            Invaild-Choice "You cannot enter a null/empty name!"
         }
-    
+        else {
+            return $data + '.jar'
+        }
     }
 }
 
-function Check-RAM-Type(){
+function Test-Version([string]$promptMessage) {
+    $data = ''
+    While ($data -eq '') {
+        $data = Read-Host -Prompt $promptMessage
+        $minVer = [version]'1.0.0'
+        $maxVer = [version]'999.999.999'
+
+        # not matching NOT 0-9 & '.' [ie - only 0-9 & '.']
+        #    must contain at least one '.'
+        #    must START with a digit
+        #    must END with a digit
+        if (($data -notmatch '[^0-9.]') -and ($data -match '\.') -and ($data[0] -match '[0-9]') -and ($data[-1] -match '[0-9]') -and ($data -ge $minVer) -and ($data -le $maxVer)) {
+            return $data
+        }
+        else {
+            $data = ''
+            Invaild-Choice "The value [$data] MUST be a version (major, minor, build) ranging between $minVer to $maxVer!"
+        }
+    }
+}
+
+function Test-Integer([string]$promptMessage) {
+    $data = ''
+    While (($null -eq $data) -or ($data -eq '') -or ($data -eq '0') -or (-not ($data -notmatch '[^\d]'))) {
+
+        [int]$data = Read-Host -Prompt $promptMessage
+        if (($null -eq $data) -or ($data -eq '') -or ($data -eq '0')) {
+            Write-Host ''
+            $data = ''
+            Invaild-Choice "The value [$data] cannot be null, empty or 0!"
+        }
+        elseif (-not ($data -notmatch '[^\d]')) {
+            Write-Output ''
+            $data = ''
+            Invaild-Choice "The value [$data] MUST be an integer!"
+        }
+        else {
+            return $data
+        }
+
+    }
+}
+
+function Test-RAM-Type() {
     $ramType = ''
-	$ValidChoices = ('M', 'G')
-	While ($ramType -notin $ValidChoices){
-		$ramType = (Read-Host -Prompt 'Would you like to use [M]egabytes or [G]igabytes?').ToUpper()
-		
-        if ($ramType -notin $ValidChoices){
+    $ValidChoices = ('M', 'G')
+    While ($ramType -notin $ValidChoices) {
+        $ramType = (Read-Host -Prompt 'Would you like to use [M]egabytes or [G]igabytes?').ToUpper()
+
+        if ($ramType -notin $ValidChoices) {
             $ramType = ''
-			Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
-		}else{
+            Invaild-Choice "INVALID CHOICE! PLEASE ENTER ONE OF THE CHOICES LISTED!"
+        }
+        else {
             return $ramType
-	    }
+        }
     }
 }
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#   BEGIN FUNCTION CALLS   #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
-Do{
+Do {
 
     Clear-Host
-    #-#-#-#-# Setup Checking here #-#-#-#-#
     If (!(Test-Path $configFileName)) {
-        Setup-Console
-    }else{
-        #Load data from JSON
-        Main-Menu
+        Set-Up-Console
+    }
+    else {
+        Invoke-Main-Menu
     }
 
-    
-    #If there are no basic files
-
-    #If there are basic files
-    #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-} Until ($quitConsole = $true)
+} Until ($QuitConsole = $true)
